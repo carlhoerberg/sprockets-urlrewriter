b15d6b49fddf6f87f16f0d6ecd74b705bc3da76d
Carl Hörberg <carl.hoerberg@gmail.com>
2012-01-31 13:34:51 +0100

    removed non asci char from my name in gemspec file

--- lib/sprockets-urlrewriter.rb
+++ lib/sprockets-urlrewriter.rb
@@ -4,7 +4,7 @@ require 'uri'
  
 module Sprockets 
   class UrlRewriter < Processor 
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
 
     def evaluate(context, locals) 
       rel = Pathname.new(context.logical_path).parent 
