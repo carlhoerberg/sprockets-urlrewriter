f020fc231312e31de1ca37b1ecc1fa63586642e9
Carl Hörberg <carl.hoerberg@gmail.com>
2012-02-25 22:25:29 +0100

    cleaned up the tests

--- spec/urlrewriter_spec.rb
+++ spec/urlrewriter_spec.rb
@@ -2,25 +2,24 @@ require 'minitest/autorun'
 require './lib/sprockets-urlrewriter'
 
 describe 'UrlRewriter' do
-  class FakeContext
-    attr_accessor :logical_path
-  end
+  let(:context) { FakeContext.new "/assets/css/jada.css" }
+  let(:rewriter) { Sprockets::UrlRewriter.new { @css } }
+  subject { rewriter.evaluate context, {} }
 
-  it 'finds relatives urls and makes them absolut' do
-    ctx = FakeContext.new
-    ctx.logical_path = "/assets/css/jada.css"
-    css = "body { background-url: url('bg.jpg?1232') }"
-    ur = Sprockets::UrlRewriter.new { css }
-    result = ur.evaluate ctx, {}
-    assert_equal "body { background-url: url(/assets/css/bg.jpg) }", result
+  it 'finds relatives urls and makes them absolute' do
+    @css = "body { background-url: url('bg.jpg?1232') }"
+    assert_equal "body { background-url: url(/assets/css/bg.jpg) }", subject
   end
 
   it 'leaves absolute URLs as is' do
-    ctx = FakeContext.new
-    ctx.logical_path = "/assets/css/jada.css"
-    css = "body { background-url: url('http://example.com/bg.jpg?1232') }"
-    ur = Sprockets::UrlRewriter.new { css }
-    result = ur.evaluate ctx, {}
-    assert_equal css, result
+    @css = "body { background-url: url('http://example.com/bg.jpg?1232') }"
+    assert_equal @css, subject
+  end
+end
+
+class FakeContext
+  attr_accessor :logical_path
+  def initialize(lpath)
+    @logical_path = lpath
   end
 end
